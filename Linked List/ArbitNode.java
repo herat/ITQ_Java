07bc71cac9c353ebc439bfcce945e7f11620edcc
Herat <hag59@cornell.edu>
2013-05-26 14:08:37 -0400

    arbit pointer

---
+++ Linked List/ArbitNode.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Herat
+ */
+public class ArbitNode extends LinkedListNode {
+    ArbitNode arbit;
+
+    public ArbitNode(int value) {
+        super(value);
+    }
+    
+}
